bec9ae5c1d54388de5d82e242bdbf95f73eada77
jinmoon23 <wlsansdlek1@me.com>
2025-06-10 13:51:34 +0900

    변수 선언하기

---
+++ BytePractice/src/main/java/PrimitiveTypeByte.java
@@ -0,0 +1,7 @@
+public class PrimitiveTypeByte {
+    public static void main(String[] args) {
+        byte bValFrom = -128;
+        byte bValTo = 127;
+        System.out.printf("from:%d to:%d", bValFrom, bValTo);
+    }
+}
